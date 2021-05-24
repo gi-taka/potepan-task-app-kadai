877bedc0f0f2514ec51052225c6b752a4e20bcf7
gi-taka <worldhistory14@gmail.com>
2021-05-24 23:37:30 +0900

    edit validates

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
 class Task < ApplicationRecord
+  validates :title, presence: true
+  validates :start_at, presence: true
+  validates :end_at, presence: true
+  validate :start_end_check
+
+  private
+
+  def start_end_check
+    errors.add(:end_at, "は開始日より前にはできません") if end_at == true && start_at == true && end_at < start_at
+  end
 end
